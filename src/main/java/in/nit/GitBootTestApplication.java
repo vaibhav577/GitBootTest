85d46b03df3f98d927f14466ee579f5783922252
vaibh <vaibh@DESKTOP-FC3V7KP>
2020-09-18 19:21:36 -0700

    jjjjjjjjjjjjjjjjjj

--- src/main/java/in/nit/GitBootTestApplication.java
+++ src/main/java/in/nit/GitBootTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitBootTestApplication {
 
+	//this is starter of spring boot
 	public static void main(String[] args) {
 		SpringApplication.run(GitBootTestApplication.class, args);
 	}
